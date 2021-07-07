30c25287cf77b57390e82966484c62acfbd08eec
Aaryan Dewan <aaryandewan@yahoo.com>
2021-07-07 22:47:35 +0530

    private route working

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from "../context/AuthContext";
 import Dashboard from "./Dashboard";
 import Signin from "./Signin";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <Switch>
-          <Route exact path="/" component={Dashboard} />
+          <PrivateRoute exact path="/" component={Dashboard} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/signin" component={Signin} />
         </Switch>
